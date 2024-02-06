7bbc966173c91439adaf879980ad26c05f37235d
estherl12 <lamaesther5@gmail.com>
2024-02-06 21:00:06 +0545

    fix: removed commented code committed

--- server.js
+++ server.js
@@ -12,20 +12,10 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-// const connection = mysql.createConnection({
-//     host:'localhost',
-//     user:'root',
-//     password:'mypassword',
-//     database:'gpkmc_college',
-//     port: '3306',
-//     connectTimeout:60000
-// });
-// if(connection){
-//     console.log("connected to db")
-// }
+
 app.use('/api/email',controller);
 app.use('/student',studentRouter);
-console.log(process.env.PASS)
+
 app.listen(port,()=>{
     console.log("Server running on port  "+port);
 })
